8fd6eddc9f15f1cd201dc29f212ffc2450261a28
Radu\BDRadu <radumotrescu@gmail.com>
2018-03-01 10:25:16 +0200

    added vsuica + my results

---
+++ vsuica/src/loadSounds.m
@@ -0,0 +1,85 @@
+load('sounds.mat')
+load('icaTest.mat')
+A(1:2,1:2);
+m1=audioread('music-1.wav');
+m2=audioread('music-2.wav');
+
+audiowrite('miii1.wav',m1,16000)
+audiowrite('miii2.wav',m2,16000)
+
+M=[m1,m2];
+%B=A(1:2,1:2);
+A=rand(numSrc, numSrc);
+M*A;
+X=A*M.';
+subplot (4, 1, 1)
+plot(m1)
+subplot (4, 1, 2)
+plot(m2)
+
+eta = 0.01;
+eta0 = eta;
+T=1000;
+num_iter=1000;
+
+%Make some random guess of mix-matrix inverse
+W = rand(size(B))./50;
+W
+
+numSrc = size(X,1);
+ta = 0.01;
+eta0 = eta;
+b = ones(numSrc,1);
+kappa=0.01;	%0.0001-no change, 0.01-good
+T=1000;
+num_iter=5000;
+
+for i=0:num_iter,
+	Y = W*X;			% predict source matrix based on guessed mix matrix
+	[delW, delmyW, delb] = wgradientbeta(eta, kappa, b, Y, W);	% gradient descent - shift by delta
+	W = W + delW;			% update W
+	%W = W + (delmyW * 0.001);			% update W
+	eta = eta0 / (1 + (i/T));	% annealing - learning rate
+	if(mod(i,100)==0),
+		b;
+		b = b + delb;
+		%fprintf('runs %d \n',i);
+		W;
+	%	corrMat = correlations(srcMat,Y)
+	%	%fflush(stdout);
+	end;
+end;
+
+
+W
+Y = W*X;				% predict source matrix based on guessed mix matrix
+
+Y = (Y - min(min(Y))) ./ (max(max(Y)) - min(min(Y)));
+Y2=Y;
+%Y2 = Y .* 2.0;
+
+subplot (4, 1, 3)
+plot(Y2(1,:))
+subplot (4, 1, 4)
+plot(Y2(2,:))
+
+
+
+audiowrite('m1.wav',Y2(1,:),16000)
+audiowrite('m2.wav',Y2(2,:),16000)
+
+
+
+%for i=0:num_iter,
+%	Y = W*X;			% predict source matrix based on guessed mix matrix
+%	[delW, mygrad] = gradient(eta, Y, W);	% gradient descent - shift by delta
+%	W = W + delW;			% update W
+%	%W = W + (mygrad * 0.001);			% update W
+%	eta = eta0 / (1 + (i/T));	% annealing - learning rate
+%	%if(mod(i,1000)==0),
+%	%	fprintf('runs %d \n',i);
+%	%	W
+%	%	corrMat = correlations(srcMat,Y)
+%	%	%fflush(stdout);
+%	%end;
+%end;
